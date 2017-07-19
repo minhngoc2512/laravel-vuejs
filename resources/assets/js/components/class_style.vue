d11fd5dc79836226261696f32673bd4f34dadc10
Minh Ngoc <ngocnm@traithivang.vn>
2017-07-19 23:36:50 +0700

    minh

---
+++ resources/assets/js/components/class_style.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+    <div v-bind:class="{active : isActive,'text-danger':hasError}" ></div>
+    <br>
+    <div v-bind="classObject" >asdf</div>
+    <div v-if="condition1"> condition true(v-if)</div>
+    <div v-else > Condition false(v-else)</div>
+    <div class="alert alert-success">
+        <h1>if-else-if</h1>
+        <div v-if="string === 'a'"> Value = a</div>
+        <div v-else-if="string === 'b'"> Value = b</div>
+        <div v-else-if="string === 'c'">Value =c</div>
+        <div v-else> Value = none</div>
+
+    </div>
+
+</div>
+</template>
+<script>
+    export default{
+        data(){
+            return{
+                isActive:true,
+                condition1:false,
+                hasError:false,
+                classObject:{
+                    active:true,
+                    'text-danger': false
+                },
+                string :'h'
+            }
+        }
+    }
+</script>
---
+++ resources/assets/js/components/list.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <div class="alert alert-danger">"V-FOR"<br>
+            <ul>
+                <li v-for="(item,index) in items" >
+                    {{item_parent}}-{{index}}- {{item.name}}-{{item.lastname}}-{{items[2]}}
+
+                </li>
+            </ul>
+
+</div>
+        <div class="alert alert-info" > <h3>V-FOR with Object</h3><br>
+            <ul>
+                <li v-for="(value,key,index) in Object1">key:{{key}}::{{index}}::{{value}}</li>
+            </ul>
+
+        </div>
+        <div class="alert alert-danger"  > Range for(n in 10): <span v-for="n in 10">{{n}}</span></div>
+        <br><div>
+        <ul>
+
+        </ul>
+    </div>
+    </div>
+
+</template>
+<script>
+    export default{
+        data(){
+            return {
+                item_parent :"Item parent",
+                items:[
+                    {name:'tom'},
+                    {name:'cat'},
+                    {lastname:'minh'}
+                ],
+                Object1:{
+                    name:'minh',
+                    last_name:'ngoc',
+                    age : 23
+                },
+                numbers:[
+                    1,2,3,4,5,6,7,8,9,10
+                ]
+
+            }
+        },method:{
+
+        }
+    }
+</script>
